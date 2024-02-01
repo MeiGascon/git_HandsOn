4a8e6fd65606975600456727deb27cde30354aab
MeiGascon <mei_gascon@MSI>
2024-02-01 10:28:08 +0100

    hemos modificado el printwhen it finds the motif

--- seqClass.py
+++ seqClass.py
@@ -28,6 +28,6 @@ if args.motif:
 	args.motif = args.motif.upper()
 	print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
 	if re.search(args.motif, args.seq):
-		print("Se ha encontrado el motif")
+		print("motif FINDED")
 	else:
 		print("No se ha encontrado el motif")
